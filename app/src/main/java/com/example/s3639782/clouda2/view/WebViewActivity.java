3a7a2b03564ffbea8ad700d55a00b71bbdd65b5d
Tejas Cherukara <taniyan@studnent.unimelb.edu.au>
2019-02-02 18:28:16 +1100

    added new webview files

---
+++ app/src/main/java/com/example/s3639782/clouda2/view/WebViewActivity.java
@@ -0,0 +1,36 @@
+package com.example.s3639782.clouda2.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.s3639782.clouda2.R;
+
+public class WebViewActivity extends AppCompatActivity{
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_analysis);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://data-analysis-dot-ccassignment2.appspot.com/chart");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+            return;
+        }
+        super.onBackPressed();
+    }
+}
